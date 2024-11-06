77740241245ee3969fcb216e60ec7c5e8b65e17f
MASSOUD <Massoud.fradi@esprit.tn>
2024-11-06 10:43:38 +0100

    hello

--- src/test/java/tn/esprit/foyer/codes/UserRepositoryTest.java
+++ src/test/java/tn/esprit/foyer/codes/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package tn.esprit.foyer.codes;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -10,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
     @Autowired
